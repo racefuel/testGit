f9b669134d1cdba18dcbf398e12c7bed7a26e1e0
Kaniska <racefuel@users.noreply.github.com>
2019-09-06 17:50:49 +0530

    modified main.py, added newFile.py

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ print(f"Well, Hello There {os.name} user!")
 
 print(" - " * 5)
 
-list1 = ("item1", "item2", "item3", "item added")
+list1 = ["item1", "item2", "item3", "item added"]
 
 print(list1)
+
+students = {"Kus", "Tina", "Alex"}
+courses = {1: "Science", 2: "Arts", 3: "Commerce"}
+
+print(courses.get(1))
